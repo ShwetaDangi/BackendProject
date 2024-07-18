889b269cb4629f9477eca24e137ae6ecaafb16cf
ShwetaDangi <127923507+ShwetaDangi@users.noreply.github.com>
2024-07-18 12:43:37 +0530

    cheaking err

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,28 +1,28 @@
-// import { apiError } from "../utils/apiError";
-// import { asyncHandler } from "../utils/asyncHandler";
-// import  Jwt  from "jsonwebtoken";
-// import { User } from "../models/user.model";
+import { apiError } from "../utils/apiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import  Jwt  from "jsonwebtoken";
+import { User } from "../models/user.model";
 
-// export const verifyJWT = asyncHandler(async(req, _, next) => {
-//     try {
-//         const token = req.cookie?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+export const verifyJWT = asyncHandler(async(req, _, next) => {
+    try {
+        const token = req.cookie?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
     
-//         if (!token){
-//             throw new apiError(400, "Unauthorised request")
-//         }
-//         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        if (!token){
+            throw new apiError(400, "Unauthorised request")
+        }
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
         
-//         const user = await User.findById(decodedToken?._id).select(" -password -refreshToken ")
+        const user = await User.findById(decodedToken?._id).select(" -password -refreshToken ")
     
-//         if(!user){
-//             //TODO: discuss about frontend 
-//             throw new apiError(401, "invalid access token")
-//         }
+        if(!user){
+            //TODO: discuss about frontend 
+            throw new apiError(401, "invalid access token")
+        }
     
-//         req.user = user;
-//         next()
-//     } catch (error) {
-//         throw new apiError(401, error?.message || "invalid access token")
-//     }
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new apiError(401, error?.message || "invalid access token")
+    }
     
-// })
+})
